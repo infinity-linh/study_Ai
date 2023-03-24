cd35a37ccd09d0566b348dd05f2a18b8c2c19de6
infinity-linh <khongnhoten312@gmail.com>
2023-03-24 21:57:51 +0700

    add_commit

--- basic/model/srcnn.py
+++ basic/model/srcnn.py
@@ -4,22 +4,24 @@ class SRCNN(nn.Module):
     def __init__(self, num_channels=3):
         super().__init__()
 
-        # Patch extraction and representation
         self.conv1 = nn.Conv2d(num_channels, 64, 9, 1, 4)
+        # self.bn1 = nn.BatchNorm2d(64)
         self.relu1 = nn.ReLU(inplace=True)
-
-        # Non-linear mapping
+        # self.relu = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(64, 32, 5, 1, 2)
+        # self.bn2 = nn.BatchNorm2d(32)
+
         self.relu2 = nn.ReLU(inplace=True)
 
-        # Reconstruction
         self.conv3 = nn.Conv2d(32, num_channels, 5, 1, 2)
-
-        # Initialize weight
+        # self.bn3 = nn.BatchNorm2d(3)
 
     def forward(self, x):
-        x = self.relu1(self.conv1(x))
-        x = self.relu2(self.conv2(x))
-        x = self.conv3(x)
-
-        return x
+        # out = self.bn1(out)
+        out = self.relu1(self.conv1(x))
+        # out = self.bn2(out)
+        out = self.relu2(self.conv2(out))
+        out = self.conv3(out)
+        # out = self.bn3(out)
+        # out += x
+        return out
